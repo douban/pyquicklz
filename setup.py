0ebd07d999ede2f3aa780f8d110c7325db9d0332
tianzhongbo <tianzhongbo@douban.com>
2016-11-18 12:31:31 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 import sys
 from setuptools import setup, Extension
-
-if sys.version_info.major < 3 and 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
-
 setup(
     name='quicklz',
     version='1.4.1',
